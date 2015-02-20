c51899c100f03df891e15c346a9f4084e0f522e2
Spinaldash <blueavenger@gmail.com>
2015-02-19 22:32:10 -0800

    product functional, requring much styling

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -8,9 +8,22 @@ module.exports = {
   handler: function(request, reply) {
     Apartment.find(function(err, apartments) {
       Client.find(function(err, clients) {
+        var data = {revenue: 0, roomsFilled: 0, roomsVacant: 0, apartmentsVacant: 0};
+        var roomsNum = 0;
+        apartments.forEach(function(a) {
+          data.revenue += a.tenants.length * a.rent;
+          data.roomsFilled += a.tenants.length
+          roomsNum += a.roomsNum
+          if (a.roomsNum !== a.tenants.length ){
+            data.apartmentsVacant++;
+          }
 
+        });
+        console.log("data.roomsFilled: ", data.roomsFilled);
+        console.log("RoomsNUM: ", roomsNum);
+        data.roomsVacant = roomsNum - data.roomsFilled;
 
-        reply.view('templates/admin/index', {path: '/admin', setActive:setActive, apartments:apartments, clients:clients});
+        reply.view('templates/admin/index', {path: '/admin', setActive:setActive, apartments:apartments, clients:clients, data:data});
       });
     })
   }
